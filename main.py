5beb76cf78b4fe0669d5e8cedc5100cd4015a0e7
giakhanh1904 <gkvlogs333@gmail.com>
2026-05-26 07:33:55 +0700

    bai

--- main.py
+++ main.py
@@ -33,31 +33,24 @@
 
 # Do đó giải pháp 2 an toàn hơn cho hệ thống y tế.
 
-# =========================================
-# HỆ THỐNG CHUẨN HÓA DỮ LIỆU SINH HIỆU
-# =========================================
-
 print("--- HỆ THỐNG TIẾP NHẬN SINH HIỆU ---")
 
-# Nhập dữ liệu
 patient_id = input("Nhập mã bệnh nhân: ")
 
 temperature_input = input("Nhập nhiệt độ cơ thể: ")
 heart_rate_input = input("Nhập nhịp tim: ")
 
-# Chuẩn hóa dữ liệu
 temperature = float(temperature_input)
 heart_rate = int(heart_rate_input)
 
-# Hiển thị kết quả
 print("\n--- KẾT QUẢ CHUẨN HÓA DỮ LIỆU ---")
 
 print("Mã bệnh nhân:", patient_id)
 
 print("Nhiệt độ cơ thể:", temperature, "độ C")
-print("⇒ Kiểu dữ liệu hệ thống ghi nhận:", type(temperature))
+print("Kiểu dữ liệu hệ thống ghi nhận:", type(temperature))
 
 print("Nhịp tim:", heart_rate, "nhịp/phút")
-print("⇒ Kiểu dữ liệu hệ thống ghi nhận:", type(heart_rate))
+print("Kiểu dữ liệu hệ thống ghi nhận:", type(heart_rate))
 
-print("Thông báo: Dữ liệu hợp lệ. Màn hình Monitor đã sẵn sàng kết nối!")
+print("Dữ liệu hợp lệ. Màn hình Monitor đã sẵn sàng kết nối!")
